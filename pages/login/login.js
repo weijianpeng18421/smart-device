254d1d461b7aa4de90e5da944875a23a58f448ee
冬天会下雨的 <784952975@qq.com>
2020-12-31 16:47:58 +0800

    制作登录界面

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,4 +1,30 @@
 Page({
-  data: {},
-  onLoad() {},
+  data: {
+    tabs: [
+      {
+        title: '验证码登录',
+      },
+      {
+        title: '账号登录',
+      }
+    ],
+
+  },
+
+  handleTabClick({ index, tabsName }) {
+    this.setData({
+      [tabsName]: index,
+    });
+  },
+  handleTabChange({ index, tabsName }) {
+    this.setData({
+      [tabsName]: index,
+    });
+  },
+  handlePlusClick() {
+    my.alert({
+      content: 'plus clicked',
+    });
+  },
+
 });
