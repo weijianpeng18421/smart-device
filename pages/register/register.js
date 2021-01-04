6a2d832d406493729d9fef304b2c19dc247221e5
冬天会下雨的 <784952975@qq.com>
2021-01-04 14:51:25 +0800

    完成登录页和注册页

---
+++ pages/register/register.js
@@ -0,0 +1,51 @@
+Page({
+  data: {
+    longPassword: '',
+    againPassword: '',
+  },
+
+  // 验证码输入
+  onItemInput(e) {
+    this.setData({
+      [e.target.dataset.field]: e.detail.value,
+    });
+  },
+  // 验证码清除
+  onClear(e) {
+    this.setData({
+      [e.target.dataset.field]: '',
+    });
+  },
+  // 验证码发送
+  onSend() {
+    my.alert({
+      title: '验证码发送成功',
+    });
+  },
+
+  // 输入密码
+  onInputPassword(e) {
+    this.setData({
+      longPassword: e.detail.value,
+    });
+  },
+  // 清除密码
+  onClearPassword() {
+    this.setData({
+      longPassword: '',
+    });
+  },
+  // 再次输入密码
+  onAgainInputPassword(e) {
+    this.setData({
+      againPassword: e.detail.value,
+    });
+  },
+  // 再次清除密码
+  onAgainClearPassword() {
+    this.setData({
+      againPassword: '',
+    });
+  },
+
+});
